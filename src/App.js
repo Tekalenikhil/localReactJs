89f20a0c2b6b1bb559e8e8c1475b97749c44a924
Tekale <nikhil.k.tekale@accenture.com>
2023-10-15 13:18:42 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+// import logo from './logo.svg';
+import './App.css';
+import{ getFirestore, collection,addDoc ,doc,getDoc} from "firebase/firestore";
+import{app} from "./firebase"
+
+const firestore = getFirestore(app)
+
+function App() {
+
+  const writeData = async()=>{
+    const result =await addDoc(collection(firestore,'cities'),{
+      name:'Mumbai',
+      pinCode:12345
+    });
+    console.log("result",result);
+  }
+  const makeSubcollection = async ()=>{
+    await addDoc(collection(firestore,'cities/640mswf0w6GQOXFVSmad/places'),{
+      name:"sinhgad",
+      desc:"college"
+    })
+  }
+
+  const getDocuments =async ()=>{
+    const ref = doc(firestore,"cities","640mswf0w6GQOXFVSmad");
+    const snap =await getDoc(ref);
+    console.log(snap.data());
+  }
+  return (
+    <div className="App">
+    <h1> hello firestore </h1>
+    <button onClick={writeData}>Put Data</button>
+    <button onClick={makeSubcollection}>Put sub Data</button>
+    <button onClick={getDocuments}>Get Data</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDu9rx3iQORKfs-_gQ6nRuaWh-sSPemCHY",
+  authDomain: "react-firestore-c003f.firebaseapp.com",
+  projectId: "react-firestore-c003f",
+  storageBucket: "react-firestore-c003f.appspot.com",
+  messagingSenderId: "27582633291",
+  appId: "1:27582633291:web:a7c91c4d428d287d6a04ce"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
